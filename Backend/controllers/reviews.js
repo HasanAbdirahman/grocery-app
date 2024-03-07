b601c0f32c08adc83101b53089966d706845c8ed
HasanAbdirahman <108181341+HasanAbdirahman@users.noreply.github.com>
2024-03-07 07:54:00 +0000

    created model controller and routes for review model

--- Backend/controllers/reviews.js
+++ Backend/controllers/reviews.js
@@ -1,19 +1,57 @@
-const Reviews= require('../models/review');
+const Review= require('../models/review');
 
-// review side
 async function getAllReviews(req, res, next){
     try{
-        let reviews = await Reviews.find({});
+        let productId = req.params.productId
+        let reviews = await Review.find({product: productId});
         if (!reviews.length)  throw new Error('No reviews available');
         res.status(200).json({success: true, reviews})
     }catch(error){
         return next(error)
     }
 }
-
-async function specificReviews(req,res, next){
-   
+async function creeateReview(req, res, next){
+    try {
+        let productId = req.params.productId
+        let review =  await Review.create({
+            user: req.params._id,
+            product:  productId,
+            rating: req.body.rating,
+            description: req.body.description
+        })
+        if(!review){
+            res.status(404).json({success:false, error:'Review is not created'})
+        }
+        res.status(201).json({ success: true, data: review });
+    } catch (error) {
+        next(error)
+    }
 }
-
-
-module.exports={specificReviews,getAllReviews}
+async function editReview(req, res, next){
+    try {
+        const reviewId = req.params.reviewId;
+        let review =await Review.findByIdAndUpdate(reviewId, {
+            rating: req.body.rating,
+            description: req.body.description
+        }, {new: true})
+        if(!review){
+            res.status(404).json({success:false, error:'Review is not edited'})
+        }
+        res.status(200).json({ success: true, data: review });
+    } catch (error) {
+        next(error)
+    }
+}
+async function deleteReview(req, res, next){
+    try {
+        const reviewId = req.params.reviewId; //  the reviewId from req.params.reviewId comes from the name that I gave at the routes same with productId
+        let review = await Review.findByIdAndDelete(reviewId)
+        if(!review){
+            res.status(404).json({success:false, error:'Review is not deleted'})
+        }
+        res.status(204).json({ success: true, data: null });
+    } catch (error) {
+        next(error)
+    }
+}
+module.exports={specificReviews,getAllReviews,creeateReview,editReview,deleteReview}
--- Backend/routes/reviews.js
+++ Backend/routes/reviews.js
@@ -3,10 +3,9 @@ const router= express.Router();
 const reviewsCtrl = require('../controllers/products')
 
 // reviews
-router.get('/product/:id/reviews', reviewsCtrl.getAllReviews);
-router.get('/product/:id/review/review:id', reviewsCtrl.specificReviews);
-router.post('/product/:id/createReview', reviewsCtrl.creeateReview);
-router.put('/product/:id/review/review:id/edit', reviewsCtrl.editReview);
-router.delete('/product/:id/review/review:id/delete', reviewsCtrl.deleteReview)
+router.get('/product/:productId/reviews', reviewsCtrl.getAllReviews);
+router.post('/product/:productId/createReview', reviewsCtrl.creeateReview);
+router.put('/product/:productId/review/:reviewId/edit', reviewsCtrl.editReview);
+router.delete('/product/:productId/review/:reviewId/delete', reviewsCtrl.deleteReview)
 
 module.exports = router
